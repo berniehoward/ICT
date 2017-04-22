91c440de2df1e89280b32e02bd31c6c939924e34
Bernie <bernie.bernie@gmail.com>
2017-04-22 16:31:58 +0300

    HC lists added to israelichild

--- israeliChild.py
+++ israeliChild.py
@@ -22,8 +22,33 @@ class IsraeliChild(Child):
         self.birthMonth = birthMonth
         self.season = season[birthMonth]
 
+        self.HCToAgeLevel1 = []
+        self.HCToAgeLevel2 = []
+        self.HCdivHeightLevel1 = []
+        self.HCdivHeightLevel2 = []
+        self.HCdivHeightSqLevel1 = []
+        self.HCdivHeightSqLevel2 = []
+        self.HCdivWeightLevel1 = []
+        self.HCdivWeightLevel2 = []
+        self.HCdivWeightSqLevel1 = []
+        self.HCdivWeightSqLevel2 = []
+
     def addBrother(self, brother):
         self.brothers.add(brother)
+
+    def calculateHeadSlops(self):
+        for x, y in zip(self.goodSamples, self.goodSamples[1:]):
+            self.HCToAgeLevel1.append(y.HC - x.HC)
+            self.HCdivHeightLevel1.append(y.HCdivHeight - x.HCdivHeight)
+            self.HCdivHeightSqLevel1.append(y.HCdivHeightSq - x.HCdivHeightSq)
+            self.HCdivWeightLevel1.append(y.HCdivWeight - x.HCdivWeight)
+            self.HCdivWeightSqLevel1.append(y.HCdivWeightSq - x.HCdivWeightSq)
+        for x, y in zip(self.goodSamples, self.goodSamples[2:]):
+            self.HCToAgeLevel2.append(y.HC - x.HC)
+            self.HCdivHeightLevel2.append(y.HCdivHeight - x.HCdivHeight)
+            self.HCdivHeightSqLevel2.append(y.HCdivHeightSq - x.HCdivHeightSq)
+            self.HCdivWeightLevel2.append(y.HCdivWeight - x.HCdivWeight)
+            self.HCdivWeightSqLevel2.append(y.HCdivWeightSq - x.HCdivWeightSq)
 
 
 
