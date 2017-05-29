a331d632d1402b0da9ee9fcf8bf990a2a883b01d
Bernie <bernie.bernie@gmail.com>
2017-05-29 10:33:25 +0300

    abs delta change

--- FirstStage/program.py
+++ FirstStage/program.py
@@ -6,17 +6,17 @@ import operator
 
 def findChildGroup(c, heights):
     for g in heights:
-        if c in g and heights.index(g) != 4:
+        if c in g and heights.index(g):
             return heights.index(g)
     return NA
 
-def scoreEpsilon(dbe, heights): #score epsilon values using manhathan distances
+def scoreEpsilonByManhattanDistances(dbe, heights): #score epsilon values using manhathan distances
     sum = 0
-    for g in dbe:
+    for g in dbe: #group in dividedByEpsilon
         for c in g:
             print(c.id, dbe.index(g), findChildGroup(c,heights))
-            sum += dbe.index(g) - findChildGroup(c,heights)
-    print(sum)
+            sum += abs(dbe.index(g) - findChildGroup(c,heights))
+    #print(sum)
     return sum
 
 def ictHeightCorrelation(epsilons, children, heights):
@@ -24,9 +24,10 @@ def ictHeightCorrelation(epsilons, children, heights):
     for e in epsilons:
         icts = [findICTByEpsilon(e, c) for c in children]
         #print(icts)
-        g1, g2, g3, g4, g_na = divideToGroups(icts, children, 7/MONTHS, 12/MONTHS, 17/MONTHS)
+        g1, g2, g3, g4, g_na = divideToGroups(icts, children, 5/MONTHS, 10/MONTHS, 15/MONTHS)
         #print("BBB", len(g1)+len(g2)+ len(g3)+ len(g4)+ len(g_na))
-        m = max(m, (scoreEpsilon([g1,g2,g3,g4], heights),e), key=operator.itemgetter(0))
+        m = max(m, (scoreEpsilonByManhattanDistances([g1,g2,g3,g4], heights),e), key=operator.itemgetter(0))
+        print("END")
     print(m[1])
     return m[1]
 
@@ -36,7 +37,7 @@ def program(dictionary):
     print(len(children))
     h1, h2, h3, h4, h_na = divideToGroups(heights, children, -2, 0, 2)
     heights = [h1, h2, h3, h4, h_na]
-    print("AAA", len(h1)+ len(h2)+ len(h3)+ len(h4)+ len(h_na))
+    print("AAA", len(h1)+ len(h2)+ len(h3)+ len(h4))
     epsilons = [x / 1000 for x in range(50, 105, 5)]
     ictHeightCorrelation(epsilons, children, heights)
 
